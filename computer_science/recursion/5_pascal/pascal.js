7f3a43c84cbb391952f989a93d2e8de4998394d7
Lixang Li <gtlx54@gmail.com>
2026-02-11 16:39:05 +1100

    Pascal finished

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,14 @@
-const pascal = function() {
-  
+const pascal = function(maxRow,currentRow = 1, prevArray = [1]) {
+    if (currentRow === maxRow) { 
+        return prevArray;
+    }
+
+    const padded = [0,...prevArray,0];
+    const nextArray = [];
+    for (let i = 0; i < padded.length-1; i++) {
+        nextArray.push(padded[i] + padded[i+1]);
+    }
+    return pascal(maxRow,currentRow+1, nextArray)
 };
   
 // Do not edit below this line
