6e22860db82e9cf884f4869fe5ba02e8555a8ade
Lixang Li <gtlx54@gmail.com>
2026-02-11 16:03:55 +1100

    permutations finished

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,4 +1,14 @@
-const permutations = function() {
+const permutations = function(array, index = 0, results = []) {
+  if (index === array.length) {
+    results.push([...array]);
+    return results;
+  }
+  for (let i = index; i < array.length; i++) {
+    [array[index],array[i]] =[array[i],array[index]];
+    permutations(array,index + 1, results);
+    [array[index],array[i]] =[array[i],array[index]];
+  }
+  return results;
   
 };
   
